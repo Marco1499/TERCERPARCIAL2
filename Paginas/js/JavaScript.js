13243896d941a5a7af474217df22c367bc3d9a85
Marcos Flores <marcos.flores@uab.edu.bo>
2022-10-31 10:23:20 -0400

    primercomit

---
+++ Paginas/js/JavaScript.js
@@ -0,0 +1,29 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/IDBRequest/success_event
+// https://developer.mozilla.org/en-US/docs/Web/API/IDBTransaction/db
+const dbConnection = window.indexedDB.open('ventas', 2);                                   // version 1
+let db;
+//on success = El successevent se dispara cuando un IDBRequesttiene éxito.
+dbConnection.onsuccess = () => {
+    db = dbConnection.result;
+    console.log("Base de datos abierta", db);
+}
+// El upgradeneededevento se activa cuando se intentó abrir una base de datos 
+// con un número de versión superior a su versión actual.
+dbConnection.onupgradeneeded = (e) => {
+    db = e.target.result;//elemento que nos devuelve
+    console.log("Crear objetos de DB", db);
+    const coleccionObjetos = db.createObjectStore('Mardeli', {
+        keyPath: 'clave'
+    });
+}
+// El errorevento se activa IDBTransactioncuando una solicitud devuelve un error 
+// y el evento aparece en el objeto de la transacción.
+dbConnection.onerror = (error) =>{
+    console.log(error);
+}
+
+//REGISTRAR AL CARRITO
+const RegistrarCompra =()=>
+{
+    alert("Registrado");
+}
